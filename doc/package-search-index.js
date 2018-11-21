1401de5b6b4667ed6b9ef5fc190e7d12052df407
Michal Krejci <michal.krejci@outlook.cz>
2018-11-21 14:38:56 +0100

    Add doc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"beer.umajkla.client"},{"l":"beer.umajkla.server"},{"l":"beer.umajkla.ui"},{"l":"beer.umajkla.ui.control"},{"l":"beer.umajkla.ui.style"},{"l":"beer.umajkla.ui.View"}]
